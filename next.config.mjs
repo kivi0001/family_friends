d7caf7ad103ef2296725de00ef3948671c38b16f
Kia <kivi0001@stud.kea.dk>
2026-04-10 14:32:47 +0200

    first setup

---
+++ next.config.mjs
@@ -0,0 +1,17 @@
+/** @type {import('next').NextConfig} */
+const nextConfig = {
+  images: {
+    remotePatterns: [
+      {
+        protocol: "https",
+        hostname: "placecats.com",
+        port: "",
+        pathname: "/neo/**",
+        search: "",
+      },
+    ],
+  },
+  /* config options here */
+};
+
+export default nextConfig;
